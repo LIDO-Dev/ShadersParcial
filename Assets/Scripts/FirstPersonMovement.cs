8af95698d2aa4e6494cbc115c41fc9a212c50cab
lautarodiazovejero <diazovejerol@gmail.com>
2024-09-17 18:55:52 -0300

    Tree-Grass

---
+++ Assets/Scripts/FirstPersonMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+
+    [Header("<color=yellow>Animator</color>")]
+    [SerializeField] private Animator _animator;
+    [SerializeField] private string _xAxisName = "xAxis";
+    [SerializeField] private string _zAxisName = "zAxis";
+    [SerializeField] private string _isMoving = "Moving";
+    private float _xAxis, _zAxis;
+    private Vector3 _dir;
+
+    [Header("Running")]
+    public bool canRun = true;
+    public bool IsRunning { get; private set; }
+    public float runSpeed = 9;
+    public KeyCode runningKey = KeyCode.LeftShift;
+
+    Rigidbody rigidbody;
+    /// <summary> Functions to override movement speed. Will use the last added override. </summary>
+    public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
+
+
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        rigidbody = GetComponent<Rigidbody>();
+
+         if(!_animator)
+        {
+            _animator = GetComponentInChildren<Animator>();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Update IsRunning from input.
+        IsRunning = canRun && Input.GetKey(runningKey);
+
+        // Get targetMovingSpeed.
+        float targetMovingSpeed = IsRunning ? runSpeed : speed;
+        if (speedOverrides.Count > 0)
+        {
+            targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
+        }
+
+        // Get targetVelocity from input.
+        Vector2 targetVelocity =new Vector2( Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
+
+        // Apply movement.
+        rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat(_xAxisName, _xAxis);
+        _animator.SetFloat(_zAxisName, _zAxis);
+        _animator.SetBool(_isMoving, (_xAxis != 0 || _zAxis != 0));
+
+        _xAxis = Input.GetAxis($"Horizontal");
+        _zAxis = Input.GetAxis($"Vertical");
+
+        _dir = (transform.right * _xAxis + transform.forward * _zAxis).normalized;
+    }
+}
